1f7faaae248484b291904d29b0324f1d0a3621a1
Lovehacking <vik.lovehacking@gmail.com>
2020-05-25 10:56:53 +0300

    Corrected Main class. Added printing top videos from ru and en channels

--- src/main/java/com/lovehacking/ytp/Main.java
+++ src/main/java/com/lovehacking/ytp/Main.java
@@ -7,6 +7,8 @@ import com.lovehacking.ytp.facade.TopSearcherFacade;
 import com.lovehacking.ytp.facade.TopSearcherFacadeImpl;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 public class Main {
 
@@ -14,8 +16,14 @@ public class Main {
         AppConfiguration app = new AppConfiguration();
         TopSearcherFacade topSearcherFacade = new TopSearcherFacadeImpl(app);
 
-        List<RatedVideoData> top = topSearcherFacade.findTopForChannels(ChannelsType.ALL, 20);
+        System.out.println("TOP 50 RU");
+        List<RatedVideoData> topRU = topSearcherFacade.findTopForChannels(ChannelsType.RU, 50);
+        AtomicInteger i = new AtomicInteger(1);
+        topRU.forEach(video -> System.out.println(i.getAndIncrement() + ") " + video.toString()));
 
-        top.forEach(System.out::println);
+        System.out.println("\nTOP 50 EN");
+        List<RatedVideoData> topEN = topSearcherFacade.findTopForChannels(ChannelsType.EN, 50);
+        AtomicInteger in = new AtomicInteger(1);
+        topEN.forEach(video -> System.out.println(in.getAndIncrement() + ") " + video.toString()));
     }
 }
